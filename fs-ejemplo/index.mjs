7aaacd36cb1402ae1743ce438c63f5835f6ce777
KordeRoys <marianogc98@outlook.com>
2026-03-06 21:56:36 -0300

    Prueba de fs rename

--- fs-ejemplo/index.mjs
+++ fs-ejemplo/index.mjs
@@ -6,6 +6,7 @@ import fs from 'fs';
 */
 
 // Leer un archivo de manera asíncrona 
+/*
 fs.readFile('./data/example.txt', 'utf8', (err, data) => { //En este caso esta direccion esta de ejemplo
     if (err) throw err;
     console.log('Contenido del archivo:', data);
@@ -16,9 +17,19 @@ fs.writeFile('./data/newfile.txt', 'Contenido nuevo', (err) => {
     if (err) throw err;
     console.log('Archivo creado y escrito');
 });
+*/
 
 // Renombrar un archivo 
-fs.rename('./data/newfile.txt', './data/renamedfile.txt', (err) => {
+function renombrarArchivo() {
+  // fs.rename es una funcion asincrona que se utiliza para renombrar un archivo o moverlo a una nueva ubicacion
+  // los parametros que necesita son:
+  // fs.rename(oldPath, newPath, callback)
+  // oldPath es la ruta actual del archivo que queremos renombrar o mover
+  // newPath es la nueva ruta o nombre del archivo
+  // callback es una funcion que se ejecuta despues de que se complete la operacion (con un posible error como argumento)
+  fs.rename("./fs-ejemplo/data/newfile.txt", "./fs-ejemplo/data/renamedfile.txt", (err) => {
     if (err) throw err;
-    console.log('Archivo renombrado');
-}); 
+    console.log("Archivo renombrado correctamente");
+  });
+}
+renombrarArchivo();
